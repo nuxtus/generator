3b6ee416aae2c106c10a8ec3d798bb5e00b34200
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-07-08 18:52:03 +0800

    Index.vue template now works

--- src/templates/index.njk.vue
+++ src/templates/index.njk.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+  import { components } from "../../interfaces/nuxtus";
+  type {$ collection $} = components["schemas"]["Items{$ collection $}"];
   const { getItems } = useDirectusItems();
   const filters = {/* Put your filters here */};
-  const items = await getItems<any>({
+  const items: {$ collection $}[] = await getItems<any>({
     collection: "{$ collection $}",
     params: {
       filter: filters,
