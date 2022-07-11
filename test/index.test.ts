fe199231c25284b948fd54a0eb4e272119de096f
Craig Harman <324026+craigharman@users.noreply.github.com>
2022-07-11 20:39:47 +0800

    test: :white_check_mark: await createPage in test
    Wait for createPage to complete before testing expectation.

--- test/index.test.ts
+++ test/index.test.ts
@@ -6,13 +6,13 @@ import path from "node:path"
 
 afterEach(() => {
 	const pageFolder = path.join("pages", "test")
-	fs.rmdirSync(pageFolder, { recursive: true })
+	fs.rmSync(pageFolder, { recursive: true })
 	const pageFolder2 = path.join("pages", "test2")
-	fs.rmdirSync(pageFolder2, { recursive: true })
+	fs.rmSync(pageFolder2, { recursive: true })
 })
 
-test("Create collection pages", () => {
-	createPage("test", false)
+test("Create collection pages", async () => {
+	await createPage("test", false)
 	expect(fs.existsSync("pages/test")).toBe(true)
 	expect(fs.existsSync("pages/test/index.vue")).toBe(true)
 	const indexPage = fs.readFileSync("pages/test/index.vue")
@@ -27,8 +27,8 @@ test("Create collection pages", () => {
 	).toBe(true)
 })
 
-test("Create singleton page", () => {
-	createPage("test2", true)
+test("Create singleton page", async () => {
+	await createPage("test2", true)
 	expect(fs.existsSync("pages/test2/index.vue")).toBe(true)
 	const indexPage = fs.readFileSync("pages/test2/index.vue")
 	expect(indexPage.includes('collection: "test2"')).toBe(true)
